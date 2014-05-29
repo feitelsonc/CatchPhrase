0c25d7321680c8af6ff2eaac0cbc290a0d28aa8c
Cory Feitelson <cfeitelson@roovy.com>
2014-05-29 00:19:40 -0700

    removed fragment
    Signed-off-by: Cory Feitelson <cfeitelson@roovy.com>

--- src/com/cs185/catchphrase/MainActivity.java
+++ src/com/cs185/catchphrase/MainActivity.java
@@ -1,29 +1,13 @@
-/*
-* Copyright 2013 The Android Open Source Project
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
-
-
-
-
 package com.cs185.catchphrase;
 
+import android.annotation.SuppressLint;
 import android.app.ActionBar;
 import android.app.Activity;
-import android.app.FragmentTransaction;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.WindowManager;
 
 /**
  * A simple launcher activity containing a summary sample description
@@ -31,39 +15,56 @@ import android.view.Menu;
  */
 public class MainActivity extends Activity {
 
-    public static final String TAG = "MainActivity";
-
-    public static final String FRAGTAG = "ImmersiveModeFragment";
-    
-    private ImmersiveModeFragment fragment;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
+        hideSystemBars();
         ActionBar actionBar = getActionBar();
         actionBar.hide();
-        
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
-
-        if (getFragmentManager().findFragmentByTag(FRAGTAG) == null ) {
-            FragmentTransaction transaction = getFragmentManager().beginTransaction();
-            fragment = new ImmersiveModeFragment();
-            transaction.add(fragment, FRAGTAG);
-            transaction.commit();
-        }
     }
     
     @Override
     protected void onResume() {
     	super.onResume();
-        fragment.hideSystemBars();
+        hideSystemBars();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
 //        getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+    
+    @SuppressLint("InlinedApi")
+	public void hideSystemBars() {
+        // Navigation bar hiding:  Backwards compatible to ICS.
+        if (Build.VERSION.SDK_INT >= 14) {
+        	getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        }
+
+        // Status bar hiding: Backwards compatible to Jellybean
+        if (Build.VERSION.SDK_INT >= 16) {
+        	getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN);
+        }
+
+        // Immersive mode: Backward compatible to KitKat.
+        // Note that there are two immersive mode UI flags, one of which is referred to as "sticky".
+        // Sticky immersive mode differs in that it makes the navigation and status bars
+        // semi-transparent, and the UI flag does not get cleared when the user interacts with
+        // the screen.
+        if (Build.VERSION.SDK_INT >= 18) {
+        	getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
+        }
     }
 
 }
